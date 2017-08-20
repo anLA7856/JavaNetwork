8534ba355a0110f4a1339bae4257b3ddf901481a
anLA7856 <350493391@qq.com>
2017-08-20 18:54:39 +0800

    提交queryString
    Signed-off-by: anLA7856 <350493391@qq.com>

---
+++ src/main/java/anla/Network/uriAndUrl/QueryString.java
@@ -0,0 +1,31 @@
+package anla.Network.uriAndUrl;
+
+import java.net.URLEncoder;
+
+public class QueryString {
+	private StringBuilder query = new StringBuilder();
+	
+	public synchronized void add(String name,String value){
+		query.append('&');
+		encode(name,value);
+	}
+	
+	public synchronized void encode(String name,String value){
+		try {
+			query.append(URLEncoder.encode(name,"UTF-8"));
+			query.append('=');
+			query.append(URLEncoder.encode(value, "UTF-8"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized String getQuery(){
+		return query.toString();
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+}
