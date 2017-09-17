bba25ce2d4ff7c21ad6346689f892c98ecd3572b
anLA7856 <350493391@qq.com>
2017-09-17 22:56:36 +0800

    解决一个小错误，使得可以执行class文件
    Signed-off-by: anLA7856 <350493391@qq.com>

--- src/main/java/anla/VM/remoteExec/TestExec.java
+++ src/main/java/anla/VM/remoteExec/TestExec.java
@@ -7,7 +7,7 @@ public class TestExec {
 	public static void main(String[] args) throws Exception{
 		InputStream is = new FileInputStream("/home/anla7856/桌面/Test.class");
 		byte[] b = new byte[is.available()];
-		is.read();
+		is.read(b);
 		is.close();
 		
 		System.out.println(JavaClassExecuter.execute(b));
